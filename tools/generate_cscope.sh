404cbf437740f0929f70c78b4a6aae0324326e05
Serapheim Dimitropoulos <serapheimd@gmail.com>
2014-12-25 05:30:37 -0600

    Begin setting up cscope

---
+++ tools/generate_cscope.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# AUTHOR: Serapheim Dimitropoulos <serapheimd@gmail.com>
+# CREATED: Thu Dec 25 05:02:09 CST 2014
+# CONTRIBUTORS:
+# UPDATED: Thu Dec 25 05:02:09 CST 2014
+
+# PURPOSE:
+# Generates cscope.files of C files and
+# headers in the current directory and
+# any subdirectories.
+
+
+find `pwd` -name "*.c" -o -name "*.h" > cscope.files
+cscope -q -R -b -i cscope.files
