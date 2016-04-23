1def1df51b5b2517a5e31e2cbd9e725565f20a42
Serapheim Dimitropoulos <serapheimd@gmail.com>
2016-04-23 14:06:23 -0500

    initcode make module

--- src/boot/module.mk
+++ src/boot/module.mk
@@ -1,11 +1,11 @@
-local_dir  := boot
-source     := bootmain.c bootasm.S entryother.S initcode.S
-local_bins := $(addprefix $(local_dir)/, bootblock entryother initcode)
-local_src  := $(addprefix $(local_dir)/,$(source))
+dir_0      := boot
+source     := bootmain.c bootasm.S entryother.S
+local_bins := $(addprefix $(dir_0)/, bootblock entryother)
+local_src  := $(addprefix $(dir_0)/,$(source))
 local_objs := $(subst .c,.o,$(filter %.c,$(local_src))) \
               $(subst .S,.o,$(filter %.S,$(local_src))) \
-	      $(local_dir)/bootblock.o \
-	      $(local_dir)/bootblockother.o
+	      $(dir_0)/bootblock.o \
+	      $(dir_0)/bootblockother.o
 
 # TODO: On the variables above rename source to local_src
 # and then just add the prefix function below? Just consider
@@ -14,53 +14,43 @@ local_objs := $(subst .c,.o,$(filter %.c,$(local_src))) \
 objects  += $(local_objs)
 sources  += $(local_src)
 binaries += $(local_bins)
-toclean  += $(local_dir)/*.asm $(local_dir)/*.out
+toclean  += $(dir_0)/*.asm
 
 # TODO: What does objcopy do?
 
-$(local_dir)/bootblock: $(local_dir)/bootblock.o
-	$(OBJDUMP) -S $< > $(local_dir)/bootblock.asm
+$(dir_0)/bootblock: $(dir_0)/bootblock.o
+	$(OBJDUMP) -S $< > $(dir_0)/bootblock.asm
 	$(OBJCOPY) -S -O binary -j .text $< $@
-	./$(local_dir)/sign.pl $@
+	./$(dir_0)/sign.pl $@
 
-$(local_dir)/entryother: $(local_dir)/bootblockother.o
+$(dir_0)/entryother: $(dir_0)/bootblockother.o
 	$(OBJCOPY) -S -O binary -j .text $< $@
-	$(OBJDUMP) -S $< > $(local_dir)/entryother.asm
+	$(OBJDUMP) -S $< > $(dir_0)/entryother.asm
 
-$(local_dir)/initcode: $(local_dir)/initcode.out $(local_dir)/initcode.o
-	$(OBJCOPY) -S -O binary -j .text $(local_dir)/initcode.out $@
-	$(OBJDUMP) -S $(local_dir)/initcode.o > $(local_dir)/initcode.asm
-
-# TODO: Find out if there is a way to not keep using $(local_dir)
+# TODO: Find out if there is a way to not keep using $(dir_0)
 
 # TODO: Putting bootasm.o after bootmain.o in the linker
 # receipe below, links an incorrect binary. Find out why
 # and document the reason!
 
-$(local_dir)/bootblock.o: $(local_dir)/bootasm.o $(local_dir)/bootmain.o
-	$(LD) $(LDFLAGS) -N -e start -Ttext 0x7C00 -o $@ $(local_dir)/bootasm.o $(local_dir)/bootmain.o
+$(dir_0)/bootblock.o: $(dir_0)/bootasm.o $(dir_0)/bootmain.o
+	$(LD) $(LDFLAGS) -N -e start -Ttext 0x7C00 -o $@ $(dir_0)/bootasm.o $(dir_0)/bootmain.o
 
 # TODO: Using $< for one dependecy is ok for the two rules
 # below but what if I have two dependencies like the rule above?
 
-$(local_dir)/bootblockother.o: $(local_dir)/entryother.o
+$(dir_0)/bootblockother.o: $(dir_0)/entryother.o
 	$(LD) $(LDFLAGS) -N -e start -Ttext 0x7000 -o $@ $<
-
-$(local_dir)/initcode.out: $(local_dir)/initcode.o
-	$(LD) $(LDFLAGS) -N -e start -Ttext 0 -o $@ $<
 
 # TODO: Not sure if I need the -I. below
 
-$(local_dir)/bootmain.o: $(local_dir)/bootmain.c
+$(dir_0)/bootmain.o: $(dir_0)/bootmain.c
 	$(CC) $(CFLAGS) -fno-pic -O -nostdinc -I. -c $< -o $@
 
 # TODO: Maybe merge .S rules?
 
-$(local_dir)/bootasm.o: $(local_dir)/bootasm.S
+$(dir_0)/bootasm.o: $(dir_0)/bootasm.S
 	$(CC) $(CFLAGS) -fno-pic -nostdinc -I. -c $< -o $@
 
-$(local_dir)/entryother.o: $(local_dir)/entryother.S
-	$(CC) $(CFLAGS) -fno-pic -nostdinc -I. -c $< -o $@
-
-$(local_dir)/initcode.o: $(local_dir)/initcode.S
+$(dir_0)/entryother.o: $(dir_0)/entryother.S
 	$(CC) $(CFLAGS) -fno-pic -nostdinc -I. -c $< -o $@
